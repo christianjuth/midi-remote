f1c9c3deb3e53c108e84a01a4a19a1eac4e25dfc
Christian Juth <cjuth2@gmail.com>
2022-10-18 18:25:11 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import "react-piano/dist/styles.css";
+import "./App.css";
+import { w3cwebsocket } from "websocket";
+// @ts-ignore
+import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
+
+function useClient(ip: string) {
+  const [client, setClient] = useState<w3cwebsocket>();
+
+  useEffect(() => {
+    if (ip) {
+      const _client = new w3cwebsocket(`ws://${ip}:3002/`, "echo-protocol");
+
+      _client.onopen = () => {
+        setClient(_client);
+      };
+
+      _client.onclose = () => {
+        setClient(undefined);
+      };
+    }
+  }, [ip]);
+
+  return client;
+}
+
+const firstNote = MidiNumbers.fromNote("c3");
+const lastNote = MidiNumbers.fromNote("f5");
+
+const keyboardShortcuts = KeyboardShortcuts.create({
+  firstNote: firstNote,
+  lastNote: lastNote,
+  keyboardConfig: KeyboardShortcuts.HOME_ROW,
+});
 
 function App() {
+  const [clientIp, setClientIp] = useState("");
+  const [ip, setIp] = useState("");
+
+  const client = useClient(clientIp);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {!client ? (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setClientIp(ip.trim());
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            onChange={(e) => setIp(e.target.value)}
+            placeholder="Local ip address of your computer"
+          />
+          <button>Set IP</button>
+        </form>
+      ) : (
+        <Piano
+          noteRange={{ first: firstNote, last: lastNote }}
+          playNote={(note: number) => {
+            client?.send(JSON.stringify({ note, velocity: 100 }));
+          }}
+          stopNote={(note: number) => {
+            console.log("STAWP");
+            client?.send(JSON.stringify({ note, velocity: 0 }));
+          }}
+          width={1000}
+          keyboardShortcuts={keyboardShortcuts}
+        />
+      )}
     </div>
   );
 }
